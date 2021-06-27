e4cb161c8e09866a727ee247dd2661fef19adc13
hiroaki.ooki <moru150606@gmail.com>
2021-06-27 08:16:44 +0000

    kadai-opp-1

--- team.rb
+++ team.rb
@@ -10,15 +10,15 @@ class Team
          return self.win.to_f/(self.win + self.lose)
       end
       def show_team_result
-        p "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{self.calc_win_rate}です。"
+        puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は #{self.calc_win_rate}です。"
       end
 end
-giants = Team.new("読売ジャイアンツ",67,45,8)
-tigers = Team.new("阪神タイガース",60,53,7)
-dragons = Team.new("中日ドラゴンズ",60,55,5)
-beysters = Team.new("横浜ベイスターズ",56,58,6)
-carp = Team.new("広島東洋カープ",52,56,12)
-swallows = Team.new("ヤクルトスワローズ",41,69,10)
+giants = Team.new("Giants",67,45,8)
+tigers = Team.new("Tigers",60,53,7)
+dragons = Team.new("Dragons",60,55,5)
+beysters = Team.new("BayStars",56,58,6)
+carp = Team.new("Carp",52,56,12)
+swallows = Team.new("Swallows",41,69,10)
 
 teams = [giants,tigers,dragons,beysters,carp,swallows]
  teams.each do |team|
